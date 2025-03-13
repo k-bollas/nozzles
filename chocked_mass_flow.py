ef065c67135d86c9cbf719802247c5e531bd0579
Kostas Bollas <kostasb@192.168.2.5>
2025-03-13 17:32:06 +0200

    2nd commit

--- chocked_mass_flow.py
+++ chocked_mass_flow.py
@@ -19,16 +19,16 @@ def main():
 
 
 
-    pt5 = 180e3
-    Tt5 = 1140
+    pt6 = 180e3
+    Tt6 = 1140
 
     NPR_r = np.linspace(1, NPR_crit, 200)
-    M5 = np.sqrt((NPR_r**((gamma-1)/gamma)-1)*(2/(gamma-1)))
-    T5 = Tt5/(1+(gamma-1)/2*M5**2)
-    p5  = pt5/NPR_r
-    rho5 = p5/(R*T5)
-    V5 = M5*np.sqrt(gamma*R*T5)
-    m_flow = rho5*A*V5
+    M6 = np.sqrt((NPR_r**((gamma-1)/gamma)-1)*(2/(gamma-1)))
+    T6 = Tt6/(1+(gamma-1)/2*M6**2)
+    p6  = pt6/NPR_r
+    rho6 = p6/(R*T6)
+    V6 = M6*np.sqrt(gamma*R*T6)
+    m_flow = rho6*A*V6
 
     fig, ax = new_figure(latex_style=True)
     scale = 0.7
@@ -41,10 +41,10 @@ def main():
     # ax.axvline(np.max(m_flow), ls='--', lw=1.5, color='k')
     ax.grid()
     ax.set_xticklabels(["0", r'$\dot{m}_{\mathrm{choke}}$'])
-    ax.set_ylabel(r'$p_{t5} / p_{\infty}$')
+    ax.set_ylabel(r'$p_{t6} / p_{\mathrm{amb}}$')
     ax.set_yticks([1, NPR_crit, 1.5*NPR_crit])
     ax.set_yticklabels(['1', r'$\mathrm{NPR}_{\mathrm{crit}}$', r'+$\infty$'])
-    mchoke = A*pt5/np.sqrt(Tt5)*np.sqrt(gamma/R)*(1+(gamma-1)/2)**(-(gamma+1)/(2*(gamma-1)))
+    mchoke = A*pt6/np.sqrt(Tt6)*np.sqrt(gamma/R)*(1+(gamma-1)/2)**(-(gamma+1)/(2*(gamma-1)))
     print(np.sqrt(gamma/R)*(1+(gamma-1)/2)**(-(gamma+1)/(2*(gamma-1))))
     # ax.scatter(mchoke, 1.2*NPR_crit)
     plt.tight_layout(pad=0.5)
@@ -59,7 +59,7 @@ def main():
     ax.set_yticks([0, np.max(m_flow)])
     ax.grid()
     ax.set_yticklabels(["0", r'$\dot{m}_{\mathrm{choke}}$'])
-    ax.set_xlabel(r'$p_{\infty} / p_{t5}$')
+    ax.set_xlabel(r'$p_{\mathrm{amb}} / p_{t6}$')
     ax.set_xticks([0, 1/NPR_crit, 1])
     ax.set_xticklabels(['0', r'$\frac{1}{\mathrm{NPR}_{\mathrm{crit}}}$', '1'])
     print(np.round(1/NPR_crit, 3))
@@ -91,5 +91,5 @@ def supersonic_flow():
 
 
 if __name__ == '__main__':
-    # main()
-    supersonic_flow()
+    main()
+    # supersonic_flow()
